8151680b1ecc60510819db6da3e947c9340b1df2
satyaprakash-04 <sahoosatyaprakash152@gmail.com>
2024-02-19 22:03:02 +0530

    code change task

--- my_app/models.py
+++ my_app/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class Customer(models.Model):
+    customer_id = models.BigAutoField(primary_key=True)
+    first_name = models.CharField(max_length=54, help_text='Customer First name')
+    last_name = models.CharField(max_length=54, help_text='Customer Last name')
+    user_email = models.EmailField(help_text='Customer Email')
+    user_phone = models.EmailField(help_text='Customer Phone')
+    password = models.CharField(max_length=544, help_text='Password')
